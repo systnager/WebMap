00632ee063002449d964caa9cbe38d15df57933d
systnager <systnager@gmail.com>
2024-01-18 10:19:27 +0200

    add functional for gps working in web view

--- app/src/main/java/com/bohdan2505/webmap/SecondFragment.kt
+++ app/src/main/java/com/bohdan2505/webmap/SecondFragment.kt
@@ -1,18 +1,18 @@
 package com.bohdan2505.webmap
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebChromeClient
-import android.webkit.WebResourceRequest
-import android.webkit.WebSettings
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.bohdan2505.webmap.databinding.FragmentSecondBinding
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -29,21 +29,41 @@ class SecondFragment : Fragment() {
     ): View {
         (activity as? AppCompatActivity)?.supportActionBar?.hide()
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
+
+        val REQUEST_CODE_PERMISSION_ACCESS_FINE_LOCATION = 100
+
+        activity?.let {
+            ActivityCompat.requestPermissions(
+                it, arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_CODE_PERMISSION_ACCESS_FINE_LOCATION
+            )
+        }
+
+        initializeWebView()
+
+        return binding.root
+    }
+
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initializeWebView() {
         val pathToHtml = arguments?.getString("html_path").toString()
         val mapWebView: WebView = binding.root.findViewById(R.id.map_web_view)
         val webSettings: WebSettings = mapWebView.settings
         mapWebView.settings.javaScriptEnabled = true
+        mapWebView.settings.setGeolocationEnabled(true)
         webSettings.allowFileAccess = true
         mapWebView.webChromeClient = WebChromeClient()
-        mapWebView.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-                view?.loadUrl(request?.url.toString())
-                return true
-            }
-        }
-        mapWebView.loadUrl("file:///$pathToHtml")
+        mapWebView.settings.databaseEnabled = true
+        mapWebView.settings.domStorageEnabled = true
+        webSettings.setGeolocationEnabled(true)
+        webSettings.setGeolocationDatabasePath(context?.filesDir?.path);
+        webSettings.javaScriptEnabled = true
+        val context: Context = binding.root.context
+        webSettings.setGeolocationDatabasePath(context.filesDir.path)
 
-        return binding.root
+        mapWebView.webChromeClient = MyClient()
+        mapWebView.loadUrl("file:///$pathToHtml")
     }
 
     override fun onDestroyView() {
@@ -55,5 +75,15 @@ class SecondFragment : Fragment() {
     override fun onResume() {
         (activity as? AppCompatActivity)?.supportActionBar?.hide()
         super.onResume()
+    }
+}
+
+
+internal class MyClient : WebChromeClient() {
+    override fun onGeolocationPermissionsShowPrompt(
+        origin: String?,
+        callback: GeolocationPermissions.Callback
+    ) {
+        callback.invoke(origin, true, false)
     }
 }
