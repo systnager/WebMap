1321921c723bfcd29ea8a1be5e4816d4bfca3d78
systnager <systnager@gmail.com>
2024-03-03 13:48:00 +0200

    fix geolocation

--- app/src/main/java/com/bohdan2505/webmap/SecondFragment.kt
+++ app/src/main/java/com/bohdan2505/webmap/SecondFragment.kt
@@ -2,31 +2,23 @@ package com.bohdan2505.webmap
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.ContentResolver
-import android.content.ContentValues
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.*
+import android.webkit.GeolocationPermissions
+import android.webkit.ValueCallback
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.bohdan2505.webmap.databinding.FragmentSecondBinding
-import java.text.SimpleDateFormat
-import java.util.*
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
@@ -57,7 +49,7 @@ class SecondFragment : Fragment() {
         return binding.root
     }
 
-    @SuppressLint("SetJavaScriptEnabled")
+    @SuppressLint("SetJavaScriptEnabled", "UnsafeWebViewClientUsage")
     private fun initializeWebView() {
         val pathToHtml = arguments?.getString("html_path").toString()
         val mapWebView: WebView = binding.root.findViewById(R.id.map_web_view)
@@ -65,41 +57,14 @@ class SecondFragment : Fragment() {
         mapWebView.settings.javaScriptEnabled = true
         mapWebView.settings.setGeolocationEnabled(true)
         webSettings.allowFileAccess = true
-        mapWebView.webChromeClient = WebChromeClient()
         mapWebView.settings.databaseEnabled = true
         mapWebView.settings.domStorageEnabled = true
         webSettings.setGeolocationEnabled(true)
         webSettings.setGeolocationDatabasePath(context?.filesDir?.path)
-        webSettings.javaScriptEnabled = true
-        val context: Context = binding.root.context
-        webSettings.setGeolocationDatabasePath(context.filesDir.path)
-
         mapWebView.webChromeClient = MyClient()
-        mapWebView.addJavascriptInterface(JavaScriptInterface(binding.root.context), "Android")
-
-        mapWebView.webChromeClient = object : WebChromeClient() {
-            override fun onShowFileChooser(
-                webView: WebView?,
-                filePathCallback: ValueCallback<Array<Uri>>?,
-                fileChooserParams: FileChooserParams?
-            ): Boolean {
-                fileUploadCallback?.onReceiveValue(null)
-                fileUploadCallback = filePathCallback
-                if (fileChooserParams?.acceptTypes?.contains("*/*") == true && fileChooserParams.isCaptureEnabled) {
-                } else {
-                    val intent = Intent(Intent.ACTION_GET_CONTENT)
-                    intent.addCategory(Intent.CATEGORY_OPENABLE)
-                    intent.type = "*/*"
-                    val chooserIntent = Intent.createChooser(intent, "Choose File")
-                    startActivityForResult(chooserIntent, FILE_CHOOSER_REQUEST_CODE)
-                }
-
-                return true
-            }
-        }
-
+        mapWebView.webViewClient = WebViewClient()
         mapWebView.loadUrl("file:///$pathToHtml")
-        //mapWebView.loadUrl("file:///android_asset/index.html")
+        // mapWebView.loadUrl("file:///android_asset/index.html")
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
